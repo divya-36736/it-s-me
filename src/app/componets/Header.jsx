654616d93ed3dcc84df83850268e28845221b5e8
Divya Kumari <23bec036@nith.ac.in>
2025-06-20 17:45:13 +0530

    Updated resume download logic

--- src/app/componets/Header.jsx
+++ src/app/componets/Header.jsx
@@ -54,13 +54,13 @@ function Header() {
             initial = {{y: 30, opacity:0}}
             whileInView={{y: 0, opacity:1}}
             transition={{duration:0.6, delay: 1}}
-            href="/myresume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex
+            href="/myresume.pdf" 
+            download 
+            className='px-10 py-3 border rounded-full border-gray-500 flex
             items-center gap-2'>my resume <img src="/downloads.png" alt="" className='w-4' /></motion.a>
 
         </div>
     </div>
-   
-    
   )
 }
 
